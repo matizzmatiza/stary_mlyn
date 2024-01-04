52160f04aebe133813047e63eaaaf6253123ea44
matizzmatiza <mateusztuczynski@icloud.com>
2024-01-04 16:19:19 +0100

    Changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,6 @@ function Header() {
         <header className="header">
             <img src={logo} alt="Logotyp Restauracja Stary Młyn" className="header--logo"/>
             <h1 className="header--title">Restauracja Stary Młyn</h1>
-            <h2 className="header--subtitle">Jarocin</h2>
         </header>
     );
 }
