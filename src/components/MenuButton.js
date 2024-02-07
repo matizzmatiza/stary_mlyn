b0422601a3403aa425fe1b6decec64f30d6fb12a
Mateusz Tuczyński <mateusztuczynski@Mateuszs-MacBook-Pro.local>
2024-02-07 22:49:02 +0100

    Added some fixes

--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -1,10 +1,11 @@
 import restaurantIcon from '../img/restaurant.svg';
 import 'animate.css';
+import MenuPDF from '../pdf/StaryMlynJarocinMenu.pdf';
 
 function MenuButton() {
   return (
     <section className="menuButton animate__animated animate__jackInTheBox">
-      <a href="#pdf" className="menuButton--button">
+      <a href={MenuPDF} className="menuButton--button">
         <img src={restaurantIcon} className='menuButton--icon' alt='Ikona Menu' />
         <span>Sprawdź Nasze Menu</span>
       </a>
